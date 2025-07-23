a3df7f44e5a1c4bb219ac53434270659b11ad127
yasickogul <yasickogul0705@gmail.com>
2025-07-24 01:36:18 +0530

    some configuration added

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/jathuPort/',
   build: {
     // GitHub Pages optimizations
     outDir: 'dist',
