9d52e8b3c1d70f363405de56e76189b6783eb439
yasickogul <yasickogul0705@gmail.com>
2025-07-28 18:43:10 +0530

    Comment out base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/jathuPort/',
+  //base: '/jathuPort/',
   build: {
     // GitHub Pages optimizations
     outDir: 'dist',
